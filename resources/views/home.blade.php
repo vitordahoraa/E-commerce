a738e451981f4ff578b6f9861046b9dff62ce2be
vitordahoraa <vitorcoelhodahora@gmail.com>
2021-11-02 22:08:54 -0300

    Commit Inicial, com o projeto já semi-implementado :wq!

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="row pt-5">
+            @foreach($merchants as $merchant)
+            <div class="d-flex justify-content-center container mt-5">
+                <div class="card p-3 bg-white"><i class="fa fa-apple"></i>
+                    <div class="about-product text-center mt-2"><img src="https://i.imgur.com/3VTaSeb.png" width="300">
+                        <div>
+                            <h4>Apple pro display XDR</h4>
+                            <h6 class="mt-0 text-black-50">Apple pro display XDR</h6>
+                        </div>
+                    </div>
+                    <div class="stats mt-2">
+                        <div class="d-flex justify-content-between p-price"><span>Pro Display XDR</span><span>$5,999</span></div>
+                        <div class="d-flex justify-content-between p-price"><span>Pro stand</span><span>$999</span></div>
+                        <div class="d-flex justify-content-between p-price"><span>Vesa Mount Adapter</span><span>$199</span></div>
+                    </div>
+                    <div class="d-flex justify-content-between total font-weight-bold mt-4"><span>Total</span><span>$7,197.00</span></div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('home');
+});
+
+Auth::routes();
+
+Route::get('/m/create', [App\Http\Controllers\MerchantController::class, 'create_view']);
+Route::post('/m/store', [App\Http\Controllers\MerchantController::class, 'store']);
+
+Route::get('/p/create', [App\Http\Controllers\ProductsController::class, 'create_view']);
+Route::post('/p/store', [App\Http\Controllers\ProductsController::class, 'store']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
