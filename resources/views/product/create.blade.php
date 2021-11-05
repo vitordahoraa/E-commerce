d03ed62c979694cdd12c9a52a0a465d09f1834b0
vitordahoraa.github.io <vitorcoelhodahora@gmail.com>
2021-11-05 14:33:08 -0300

    Finalizado os comentários do código

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -26,7 +26,6 @@
                                 @enderror
                             </select>
                         </div>
-                        
                         <div class="form-group row">
                             <label for="product_status" class="col-md-4 col-form-label text-md-right">{{ __('Status do produto') }}</label>
 
